1af31a9cf2e34c77c578b5d3b0b36b4c02ad6909
yisyuan <yisyuan0224@gmail.com>
2021-11-30 15:19:27 +0800

    3rd version

--- GitTest/src/Gitcode/Codeing.java
+++ GitTest/src/Gitcode/Codeing.java
@@ -5,6 +5,7 @@ public class Codeing {
 	public static void main(String[]args) {
 		
 		System.out.println("2nd");
+		System.out.println("¸t½Ï¸`¨Ó©Ô~~~~~");
 		
 		
 	} 
--- GitTest/src/Gitcode/df.java
+++ GitTest/src/Gitcode/df.java
@@ -2,4 +2,11 @@ package Gitcode;
 
 public class df {
 
+public static void main(String[]args) {
+		
+		System.out.println("2nd");
+		System.out.println("ª÷·¾­I~~~~~");
+		
+		
+	} 
 }
