fd55b644966a530c5cbdbbbb5e83f58bbe11f4e7
yisyuan <yisyuan0224@gmail.com>
2021-11-30 15:37:16 +0800

    add

--- GitTest/src/Gitcode/df.java
+++ GitTest/src/Gitcode/df.java
@@ -7,7 +7,7 @@ public static void main(String[]args) {
 		System.out.println("2nd");
 		System.out.println("金溝背~~~~~");
 		
-//		Math m = new Math();
+		Math m = new Math();
 		System.out,println(m.add(10,5));
 		System.out,println("我最喜歡的聖誕節");
 		
